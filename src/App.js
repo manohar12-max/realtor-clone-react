bb2f28c6cc1a1265a66487508a79f7eb15bb0a3d
Manohar <2898manohar@gmail.com>
2024-04-21 11:38:03 +0530

    created-navigation to menu bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignIn from "./pages/SignIn";
-
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
-
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
     <Router>
+      <Header/>
      <Routes>
        <Route path="/" element={<HomePage/>} />
        <Route path="/sign-in" element={<SignIn/>} />
